0cce83f462b32a08e54c2419c2e25123aa9cc738
John Boyes <jboyes@btsedge.com>
2023-02-21 14:23:50 -0500

    Initial commit

---
+++ src/data/timeline.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Sr Manager of Projects and Technology',
+        duration: '2 Years',
+        details: '',
+    },
+    {
+        year: '2017',
+        title: 'IT Manager',
+        duration: '4 Years',
+        details: '',
+    },
+    {
+        year: '2015',
+        title: 'IT Specialist',
+        duration: '2 Years',
+        details: '',
+    },
+    {
+        year: '2013',
+        title: 'Laboratory Technician',
+        duration: '2 Years',
+        details: '',
+    },
+]
