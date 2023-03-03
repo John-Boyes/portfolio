95cd0498ecb16b6a138e7d6c811ca69d65b09ac0
John Boyes <jboyes@btsedge.com>
2023-03-02 22:07:40 -0500

    minor updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function classNames(...classes) {
 
 export default function Navbar(props) {
   return (
-    <Disclosure as="nav" className="sticky top-0 z-50 bg-gray-800">
+    <Disclosure as="nav" className="sticky top-0 z-50 bg-gray-800 border-b border-purple-600 dark:border-orange-500">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -35,7 +35,7 @@ export default function Navbar(props) {
                 <div className="flex flex-shrink-0 items-center">
                     <a href='#'>
                         <span
-                        className='dark:text-orange-300 text-white'>
+                        className='dark:text-orange-300 text-white font-bold'>
                             JohnBoyes.dev
                         </span>
                     </a>
