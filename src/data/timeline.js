81bd47f102db012d90fcc229271e8c6b65e48634
John Boyes <info@johnboyes.dev>
2023-03-14 11:42:52 -0400

    Updated data/timeline.details

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,24 +3,24 @@ export default [
         year: 'Today',
         title: 'Sr Manager of Projects and Technology',
         duration: '2 Years',
-        details: '',
+        details: 'Migrated SharePoint apps to Teams as well as created new PowerApps and PowerAutomate flows. Migrated Twilio application from Excel and sheets to a PowerApp. Migrated from SugarCRM to Salesforce. Installed and configured Ubiquity security cameras, NVR, APs, and access control. Managed the transistion to remote work during the pandemic while simutaneously migrating from on-prem to the cloud. Created global automated tools for managing the continuous improvement projects and resources.',
     },
     {
         year: '2017',
         title: 'IT Manager',
         duration: '4 Years',
-        details: '',
+        details: 'Worked extensively on SharePoint custom applications and workflows. Migrated from SAP CRM to SugarCRM Cloud. Set up and configured a global on prem SugarCRM instance. Installed Starleaf/ClickShare conference room systems. Installed Cisco Wifi controller and 18 APs with AD integration. Managed local QlikSense applications linked to SAP, SugarCRM, and excel workbooks. Installed and configured Hikvision security cameras and NVR. Implemented LiquidPlanner for operations, engineering, and service project management. Championed a local continuous improvement initiative started by our parent company in Germany.',
     },
     {
         year: '2015',
         title: 'IT Specialist',
         duration: '2 Years',
-        details: '',
+        details: 'Managed on prem AD, file servers, print servers, IT budgets and procurement, service contracts, SAP users, and migrated company intranet to SharePoint 2013. Created the Twilio Emergency Response app for automating ringdown calls and sms.',
     },
     {
         year: '2013',
         title: 'Laboratory Technician',
         duration: '2 Years',
-        details: '',
+        details: 'Ran particle size reduction testing, used Horiba and Malvern Instruments partical size analyzers, wrote test reports and communicated findings with customers. Was also in charge of laboratory supplies procurement and hazardous shipping. Occasional updates to Allen Bradley PLC, HMI, and Drive programming.',
     },
 ]
