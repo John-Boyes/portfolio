2a389bfcf88943cde5b0784b46652dff8ae2c10b
John Boyes <info@johnboyes.dev>
2023-03-15 16:20:33 -0400

    created Passions drop down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,12 @@ const navigation = [
   { name: 'Contact', href: '#Contact', current: false },
 ]
 
+const passions = [
+  { name: 'TPA', href: 'https://transplantalliance.org', target: '_blank' },
+  { name: 'USA Vehicle Import Laws', href: 'https://transplantalliance.org', target: "_blank" },
+  { name: 'Ready Viewer One Podcast', href: 'https://readyviewerone.buzzsprout.com/', target: "_blank" },
+]
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
@@ -65,15 +71,65 @@ export default function Navbar(props) {
                         {item.name}
                       </a>
                     ))}
+        
+                <Menu as="div" className="relative inline-block text-left">
+                  <div>
+                    <Menu.Button className="text-gray-300 rounded-md px-3 py-2 text-sm font-medium ">
+                      Passions
+                    </Menu.Button>
+                  </div>
 
-                    <a
-                        key='Charity'
-                        href='https://www.transplantalliance.org'
-                        target='_blank'
-                        className='text-gray-300 rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white'>
-                            Charity
-                        </a>
-
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md
+                      bg-stone-200 dark:bg-gray-700 dark:text-white shadow-lg shadow-purple-500 dark:shadow-orange-500 ring-1 ring-black ring-opacity-5 
+                      focus:outline-none">
+                      
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="https://transplantalliance.org"
+                              target="_blank"
+                              className={classNames(active ? 'bg-gray-900' : '', 'hover:bg-gray-300 dark:hover:bg-gray-600 block px-4 py-2 text-sm ')}
+                            >
+                              Transplant Alliance 501(c)(3)
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(active ? 'bg-gray-900' : '', 'text-gray-600 hover:bg-gray-300 dark:hover:bg-gray-600 block px-4 py-2 text-sm ')}
+                            >
+                              USA Vehicle Import Laws
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="https://readyviewerone.buzzsprout.com/"
+                              target="_blank"
+                              className={classNames(active ? 'bg-gray-900' : '', 'hover:bg-gray-300 dark:hover:bg-gray-600 block px-4 py-2 text-sm ')}
+                            >
+                              Ready Viewer One Podcast
+                            </a>
+                          )}
+                        </Menu.Item>
+                        
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
                   </div>
                 </div>
               </div>
@@ -106,7 +162,7 @@ export default function Navbar(props) {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right 
-                      rounded-md bg-stone-200 dark:bg-gray-800 dark:text-white shadow-lg shadow-purple-500 dark:shadow-orange-500 ring-1 ring-black ring-opacity-5 
+                      rounded-md bg-stone-200 dark:bg-gray-700 dark:text-white shadow-lg shadow-purple-500 dark:shadow-orange-500 ring-1 ring-black ring-opacity-5 
                       focus:outline-none ">
                       
                       <Menu.Item>
