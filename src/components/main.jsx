c459376977c9e7dde829568fa1d7432e42cef6f9
John Boyes <jboyes@btsedge.com>
2023-03-19 21:47:08 +0100

    updated timeline styles

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -13,7 +13,7 @@ function Main() {
   return (
     <>
       <div className="
-        bg-gradient-to-br from-sky-100 to-sky-50
+        bg-gradient-to-bl from-sky-50 via-rose-100 to-sky-500
         dark:from-stone-800 dark:to-sky-900 text-stone-900 
              dark:text-stone-300 min-h-screen font-inter mx-auto ">
         <div className='max-w-5xl w-11/12 mx-auto'>
