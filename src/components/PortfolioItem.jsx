5459dca281b1c0dff4564f56095444018fc2ddf1
John Boyes <jboyes@btsedge.com>
2024-05-19 11:00:31 -0400

    updated card styling and fixed min h

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -11,8 +11,8 @@ function PortfolioItem({ title, imgUrl, stack, link, blurb}) {
         <button
             onClick={() => setOpen(true) }
             rel="noopener noreferrer"
-            className='bg-gray-50 dark:bg-black border-2 border-stone-900 
-                dark:border-white rounded-md overflow-hidden shadow-sm 
+            className='bg-gray-50 dark:bg-gray-700 border-2 border-stone-900 
+                dark:border-gray-400 rounded-md overflow-hidden shadow-sm 
                 shadow-purple-500 dark:shadow-orange-500'
             >
         <img 
@@ -20,12 +20,15 @@ function PortfolioItem({ title, imgUrl, stack, link, blurb}) {
             alt="portfolio"
             className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
-        <div className="w-full p-4">
+        <div className="w-full min-h-full p-4
+            bg-gradient-to-b from-slate-400 via-slate-300 via-slate-300 to-slate-400/80
+            dark:bg-gradient dark:from-gray-800 dark:via-gray-700 dark:via-gray-700 dark:to-gray-700/80
+        ">
             <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold' dark:text-white>{title}</h3>
             <p className='flex flex-wrap gap-2 flex-row items-center justify-start 
                 text-xs md:text-sm dark:text-white dark:black'>
                 {stack.map(item => (
-                    <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 bg-white dark:bg-black dark:border-white rounded-md'>
+                    <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 bg-white dark:bg-gray-500 dark:border-gray-400 rounded-md'>
                         {item}
                     </span>
                 ))}
