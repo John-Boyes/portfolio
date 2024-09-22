a56a4b0f1785a4c521123b2209e57456be64da1f
John Boyes <john@johnboyes.dev>
2024-09-22 01:57:14 -0400

    Fixed overflow for imports page

--- src/components/StateItem.jsx
+++ src/components/StateItem.jsx
@@ -20,7 +20,7 @@ export async function loader({ params }) {
 
     return (
         <ol className="container flex flex-col relative border-l border-stone-200 dark:border-stone-700">
-            <li className="mb-10 ml-4">
+            <li className="mb-10 ml-4 overflow-y-auto">
                 <div className="absolute w-3 h-3 bg-stone-200 rouded-full mt-1.5 -left-1.5 
                         border border-white dark:border-stone-900 dark:bg-stone-700" />
                 <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
@@ -32,7 +32,7 @@ export async function loader({ params }) {
                     </h3>
                 </p>
                 <div className='container p-1 m-1'>
-                    <p className='flex-row my-2 text-base font-normal p-2 m-2 whitespace-pre-wrap'
+                    <p className='flex-row my-2 text-base font-normal p-2 m-2 whitespace-pre-wrap text-wrap'
                         dangerouslySetInnerHTML={{__html: state.content.rendered}}
                         >
                     </p>
