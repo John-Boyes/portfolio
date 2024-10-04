4d0f74552a6860dac76b5c3911a0295654d5419c
John Boyes <john@johnboyes.dev>
2024-10-04 15:57:25 -0400

    update overflow on state items

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -1,16 +1,18 @@
 export default function Disclaimer() {
 
     return (
-      <div id="zero-state">
-        <div className="mx-auto mb-5">
+      <div id="zero-state ">
+        <div className="container max-h-full overflow-y-auto text-wrap">
+        <div className="flex mx-auto mb-5">
             <img src="https://johnboyes.dev/assets/acty.jpeg"
-            className="mx-auto max-w-xl"
+            className="flex w-1/2 rounded-xl mx-auto "
             />
         </div>
-        <div>
+        <div className="container whitespace-pre-wrap text-wrap">
             <p>
             This document does not constitute legal advice. It is an aggregation of resources found on the internet and should be used as a starting point for you, the reader’s personal research. When researching, be sure to cycle terms like “import”, “foreign vehicle”, “out of country”, “mini-truck”, “mini truck” and “keijidōsha” “kei” as each state uses different wording in their documents.
             </p>
+        </div>
         </div>
       </div>
     );
