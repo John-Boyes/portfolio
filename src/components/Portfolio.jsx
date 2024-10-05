a1a72dd9a452a8d4c17a942e51acf15a0b616c4f
John Boyes <john@johnboyes.dev>
2024-10-05 15:56:06 -0400

    updated styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import portfolio from '../data/portfolio';
 import PortfolioItem from './PortfolioItem';
 
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
 function Portfolio() {
   return (
-    <div className='flex flex-col md:flex-row items-center justify-center scroll-p-8 '>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
-            {portfolio.map(project => (
+    <div className='flex flex-col md:flex-row items-center justify-center scroll-p-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {portfolio.map((project, index) => (
                 <PortfolioItem
+                    key={index}
                     imgUrl={project.imgUrl}
                     title={project.title}
                     stack={project.stack}
                     link={project.link}
                     blurb={project.blurb}
-                    className='shadow-sm shadow-violet-900'
                 />    
             ))}
         </div>
